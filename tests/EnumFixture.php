b0ceb262faed732a5fda4021f8a49937a2b21e8b
Anatolii Husarov <gusarovjunior@gmail.com>
2020-07-08 22:50:49 +0300

    Update EnumFixture.php

--- tests/EnumFixture.php
+++ tests/EnumFixture.php
@@ -1,24 +1,15 @@
 <?php
 /**
- * @link    http://github.com/myclabs/php-enum
- * @license http://www.opensource.org/licenses/mit-license.php MIT (see the LICENSE file)
+ * @link    https://github.com/gusarov112/php-enum
+ * @license https://www.opensource.org/licenses/mit-license.php MIT (see the LICENSE file)
  */
 
-namespace MyCLabs\Tests\Enum;
+namespace Gusarov112\Tests\Enum;
 
-use MyCLabs\Enum\Enum;
+use Gusarov112\Enum\Enum;
 
 /**
  * Class EnumFixture
- *
- * @method static EnumFixture FOO()
- * @method static EnumFixture BAR()
- * @method static EnumFixture NUMBER()
- *
- * @method static EnumFixture PROBLEMATIC_NUMBER()
- * @method static EnumFixture PROBLEMATIC_NULL()
- * @method static EnumFixture PROBLEMATIC_EMPTY_STRING()
- * @method static EnumFixture PROBLEMATIC_BOOLEAN_FALSE()
  *
  * @author Daniel Costa <danielcosta@gmail.com>
  * @author Mirosław Filip <mirfilip@gmail.com>
